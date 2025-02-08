549c9234a63e031cc4bbb22491718cd9d2213069
Henry <henryochieng8@gmail.com>
2025-02-08 04:03:24 +0300

    Header commits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import image from '../assets/worldwide.png'
+
 export default function Header () {
     return (
         <header>
-            <h1>This is the header</h1>
+            <img src = { image } alt = "globe image" />
+            <h1>My Travel Journal</h1>
         </header>
     )
 }
